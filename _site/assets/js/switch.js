02f34f990ef8f1dbd11dd9533f0ee7761a0d448a
mancap314 <egregius314@gmail.com>
2019-02-24 21:12:38 +0100

    hamburger menu for small screens

--- _site/assets/js/switch.js
+++ _site/assets/js/switch.js
@@ -1,5 +1,6 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function switchMenu() {
+function switchMenu(x) {
+  x.classList.toggle("change");
   var x = document.getElementsByClassName("categories")[0];
   if (x.style.display === "block") {
     x.style.display = "none";
