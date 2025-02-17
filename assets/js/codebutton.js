184ec6ec97ca60979b507348dc3119d35484864d
mancap314 <egregius314@gmail.com>
2025-02-17 22:55:09 +0100

    minor changes for mobile version

---
+++ assets/js/codebutton.js
@@ -0,0 +1,24 @@
+// assets/js/post.js
+var codeBlocks = document.querySelectorAll('code');
+
+codeBlocks.forEach(function (codeBlock) {
+  var copyButton = document.createElement('button');
+  copyButton.className = 'copy';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerText = 'Copy';
+
+  codeBlock.prepend(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    var code = codeBlock.querySelector('table').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerText = 'Copied';
+    var fourSeconds = 4000;
+
+    setTimeout(function () {
+      copyButton.innerText = 'Copy';
+    }, fourSeconds);
+  });
+});
